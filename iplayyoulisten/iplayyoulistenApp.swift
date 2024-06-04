2ef0a5c16e22ce2d21cbda352bc08ec38c277411
Jed Kutai <jedkutai@gmail.com>
2024-06-04 16:10:20 -0400

    Initial Commit

---
+++ iplayyoulisten/iplayyoulistenApp.swift
@@ -0,0 +1,17 @@
+//
+//  iplayyoulistenApp.swift
+//  iplayyoulisten
+//
+//  Created by Jed Kutai on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct iplayyoulistenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
